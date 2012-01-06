29e7947aeaac86739c46c13778e0b998ff94b42c
Gustavo Rod. Baldera <gbaldera@gmail.com>
2012-01-05 20:29:48 -0400

    Added Pyrocache support, removed short tags from display.php

--- google_weather.php
+++ google_weather.php
@@ -34,16 +34,26 @@ class Widget_Google_weather extends Widgets
 
     public function run($options)
     {
-        if (empty($options['lang'])) {
+        if (empty($options['lang']))
+        {
             $options['lang'] = 'en';
         }
 
-        $city = urlencode($options['location']);
-        $content = utf8_encode(file_get_contents('http://www.google.com/ig/api?weather=' . $city . '&hl=' . $options['lang']));
+        //caching
+        if (!$content = $this->pyrocache->get('weather-' . url_title($options['location'])))
+        {
+            $city = urlencode($options['location']);
+            $content = utf8_encode(file_get_contents('http://www.google.com/ig/api?weather=' . $city . '&hl=' . $options['lang']));
+
+            //write cache and expires in 12 hours
+            $this->pyrocache->write($content, 'weather-' . url_title($options['location']), 43200);
+        }
+
         $xml = simplexml_load_string($content);
 
         // Fail?
-        if ($xml === false) {
+        if (!$xml)
+        {
             echo 'Error trying to contact the Google Weather server.';
             return false;
         }
--- views/display.php
+++ views/display.php
@@ -9,11 +9,11 @@
 <h2>Forecast</h2>
 <?php foreach ($forecast_list as $forecast): ?>
 <div class="weather">
-    <img src="http://www.google.com<?=$forecast->icon['data']?>" alt="weather">
-    <div><?=$forecast->day_of_week['data']?></div>
+    <img src="http://www.google.com<?php echo $forecast->icon['data']?>" alt="weather">
+    <div><?php echo $forecast->day_of_week['data']?></div>
             <span class="condition">
-                    <?=$forecast->low['data']?>&deg; C - <?=$forecast->high['data']?>&deg; C,
-                <?=$forecast->condition['data']?>
+                    <?php echo $forecast->low['data']?>&deg; C - <?php echo $forecast->high['data']?>&deg; C,
+                <?php echo $forecast->condition['data']?>
             </span>
 </div>
 <?php endforeach; ?>
